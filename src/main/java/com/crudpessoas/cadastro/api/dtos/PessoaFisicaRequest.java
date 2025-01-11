d44dbc2bac85a6962557991b2b4196a9bf58b702
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-01-11 11:45:56 -0300

    update PessoaFisica dto

--- src/main/java/com/crudpessoas/cadastro/api/dtos/PessoaFisicaRequest.java
+++ src/main/java/com/crudpessoas/cadastro/api/dtos/PessoaFisicaRequest.java
@@ -1,4 +1,50 @@
 package com.crudpessoas.cadastro.api.dtos;
 
-public record PessoaFisicaRequest (){
+import com.crudpessoas.cadastro.domain.entities.PessoaFisica;
+import com.crudpessoas.cadastro.domain.entities.Telefone;
+import com.crudpessoas.cadastro.domain.entities.enums.Sexo;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record PessoaFisicaRequest (
+        @NotBlank
+        String cpf,
+        @NotBlank
+        String rg,
+        @NotBlank
+        String primeiroNome,
+        @NotBlank
+        String sobrenome,
+        @NotNull
+        @NotBlank
+        Sexo sexo,
+        @NotNull
+        @NotBlank
+        Set<String> emails,
+        @NotNull
+        Set<Telefone> telefones,
+        @NotNull
+        LocalDate dataNascimento
+){
+    public static PessoaFisicaRequest toEntidade(PessoaFisica pessoaFisica){
+            if (pessoaFisica==null){
+                    return null;
+            }
+            return new PessoaFisicaRequest(
+                    pessoaFisica.getCpf(),
+                    pessoaFisica.getRg(),
+                    pessoaFisica.getPrimeiroNome(),
+                    pessoaFisica.getSobrenome(),
+                    pessoaFisica.getSexo(),
+                    pessoaFisica.getEmails(),
+                    pessoaFisica.getTelefones(),
+                    pessoaFisica.getDataNascimento()
+            );
+    }
 }
