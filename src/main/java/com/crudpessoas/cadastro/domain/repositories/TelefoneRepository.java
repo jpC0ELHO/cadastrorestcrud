4db034e94ff38ab33b6470283a5059b6ea5ca625
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-01-16 17:08:13 -0300

    update TelefoneRepository.java

--- src/main/java/com/crudpessoas/cadastro/domain/repositories/TelefoneRepository.java
+++ src/main/java/com/crudpessoas/cadastro/domain/repositories/TelefoneRepository.java
@@ -5,7 +5,9 @@ import com.crudpessoas.cadastro.domain.entities.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface TelefoneRepository extends JpaRepository<Telefone, String> {
+public interface TelefoneRepository extends JpaRepository<Telefone, UUID> {
+    Optional<Telefone>findByid(UUID uuid);
     Optional<Telefone>findByNumber(String number);
 }
